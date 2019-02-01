bdbe132a6cf321264c7cf6c8b93820b384566cf1
SiiiDd1998 <siddheshpatel1999@gmail.com>
2019-02-01 19:33:36 +0530

    smoke test done

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.25;
+
+contract Election {
+    //Constructor
+    //Read candidate
+    string public candidate;
+    //Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
