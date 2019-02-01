45fbd644738fa2a74c21e835bde3b4c143102757
SiiiDd1998 <siddheshpatel1999@gmail.com>
2019-02-02 00:12:12 +0530

    Election candidate added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,20 @@
 pragma solidity >=0.4.25;
 
 contract Election {
-    //Constructor
-    //Read candidate
-    string public candidate;
-    //Constructor
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
